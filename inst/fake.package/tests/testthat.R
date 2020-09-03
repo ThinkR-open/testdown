2364877a2a8ab28b540279337136db6c6815d612
colin <colin@thinkr.fr>
2020-09-03 10:40:16 +0200

    removed unused example

--- inst/fake.package/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(dummypackage)
-
-test_check("dummypackage")
--- inst/fake.package/tests/testthat/test-my_mean.R
+++
@@ -1,4 +0,0 @@
-test_that("my_mean works", {
-  expect_equal(my_mean(c(1, 6)), 3.5)
-  expect_equal(my_mean(c(1, 6, NA)), 3.5)
-})
