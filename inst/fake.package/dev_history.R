61aa6b57193c9fdc5fd6dee4767ce6946b42dd35
colin <colin@thinkr.fr>
2020-09-03 13:44:58 +0200

    restored fake.package for test

---
+++ inst/fake.package/dev_history.R
@@ -0,0 +1,11 @@
+# Hide this file
+usethis::use_build_ignore("dev_history.R")
+# License
+usethis::use_mit_license("ThinkR")
+
+# Documentation
+usethis::use_r("my_mean")
+usethis::use_test("my_mean")
+
+# Dependencies
+attachment::att_amend_desc()
